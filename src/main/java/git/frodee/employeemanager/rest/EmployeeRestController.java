dea1705135b5e8298e2fbfa0571555cb6bcc1e8b
FrodeE <frode.ester@gmail.com>
2023-08-26 18:03:22 +0300

    Refactor code, change endpoints

---
+++ src/main/java/git/frodee/employeemanager/rest/EmployeeRestController.java
@@ -0,0 +1,52 @@
+package git.frodee.employeemanager.rest;
+
+
+import git.frodee.employeemanager.model.Employee;
+import git.frodee.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// As we are dealing with REST service, we need to set up REST controller
+@RestController
+@RequestMapping("/api/v1/employee")
+public class EmployeeRestController {
+    private final EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> addNewEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        Employee updatedEmployee = employeeService.updateEmployee(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
+
